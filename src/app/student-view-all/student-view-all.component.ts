6c30f41c851d3e39f66cb04e78e432e9a52f7acc
Sayanaann <alexsayana@gmail.com>
2022-11-24 22:57:00 +0530

    view

---
+++ src/app/student-view-all/student-view-all.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-view-all',
+  templateUrl: './student-view-all.component.html',
+  styleUrls: ['./student-view-all.component.css']
+})
+export class StudentViewAllComponent {
+
+}
